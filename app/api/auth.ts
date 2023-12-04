164409a9e3e02f6e6a4e4494c786fcd4fcb46822
Beckett <133655553+severian42@users.noreply.github.com>
2023-12-04 16:12:21 -0500

    Update auth.ts

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -1,26 +1,11 @@
 import { NextRequest } from "next/server";
 import { getServerSideConfig } from "../config/server";
-import md5 from "spark-md5";
-import { ACCESS_CODE_PREFIX } from "../constant";
-
-function getIP(req: NextRequest) {
-  let ip = req.ip ?? req.headers.get("x-real-ip");
-  const forwardedFor = req.headers.get("x-forwarded-for");
-
-  if (!ip && forwardedFor) {
-    ip = forwardedFor.split(",").at(0) ?? "";
-  }
-
-  return ip;
-}
 
 function parseApiKey(bearToken: string) {
   const token = bearToken.trim().replaceAll("Bearer ", "").trim();
-  const isOpenAiKey = !token.startsWith(ACCESS_CODE_PREFIX);
 
   return {
-    accessCode: isOpenAiKey ? "" : token.slice(ACCESS_CODE_PREFIX.length),
-    apiKey: isOpenAiKey ? token : "",
+    apiKey: token,
   };
 }
 
@@ -28,4 +13,5 @@ export function auth(req: NextRequest) {
   return {
     error: false,
   };
+};
 }
